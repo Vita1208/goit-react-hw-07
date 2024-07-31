f82867f3de385e801a51cb3aa2dc43d2896faa10
Vita1208 <111512978+Vita1208@users.noreply.github.com>
2024-07-31 21:22:36 +0300

    react 7

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,33 +1,15 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { contactsReducer } from './contactsSlice';      
+import { configureStore } from '@reduxjs/toolkit';
+
+import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filtersSlice';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filters: filterReducer,
-});
-
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-
 const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+  reducer: {
+    contacts: contactsReducer,
+    filters: filterReducer,
+  },
 });
 
-const persistor = persistStore(store);
+export { store };
 
-export { store, persistor };
 
